baac4b84e5df5d2c2acd38abc334f68bf9aa713c
Nikita Zhuravlev <142427989+TheCLub27@users.noreply.github.com>
2023-08-28 06:48:41 +0300

    Add files via upload

---
+++ controllers/PhonebookController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\web\Response;
+use app\models\TestPeople;
+use app\models\TestPhonesNumber;
+use yii\rest\ActiveController;
+use yii\data\ActiveDataProvider;
+
+class PhoneBookController extends ActiveController{
+
+    public $modelClass = 'app\models\TestPeople';
+
+    public function actionIndex()
+    {
+        $model = TestPeople::find()->all();
+
+        return $this->render('index', [
+        'model' => $model,
+    ]);
+    }
+    
+
+    public function actions()
+    {
+        $actions = parent::actions();
+        unset($actions['create'], $actions['update'], $actions['delete']);
+        return $actions;
+    }
+
+    public function actionCreate(){
+        $model = new TestPeople();
+        $model->load(Yii::$app->getRequest()->getBodyParams(), '');
+
+        if ($model->safe()) {
+            return ['status' => 'success', 'data' => $model];
+        } else {
+            return ['status' => 'error', 'data' => $model->getErrors()];
+        }
+    }
+
+    public function actionUpdate($id){
+        $model = TestPeople::findOne($id);
+
+        if (!$model) {
+            throw new \yii\web\NotAcceptableHttpException("Человек с таким идентификатором ($id) не найден");
+        }
+
+        $model->load(Yii::$app->getRequest()->getBodyParams(), '');
+
+        if ($model->safe()) {
+            return ['status' => 'success', 'data' => $model];
+        } else {
+            return ['status' => 'error', 'data' => $model->getErrors()];
+        }
+    }
+
+    public function actionView($id){
+        $model = TestPeople::findOne($id);
+
+        if (!$model) {
+            throw new \yii\web\NotAcceptableHttpException("Человек с таким идентификатором ($id) не найден");
+        }
+
+        $phoneNumbers = TestPhonesNumber::find()->where(['person_id' => $id])->all();
+        return ['status' => 'success', 'data' => $model, 'phone_numbers' => $phoneNumbers];
+    }
+
+    public function actionDelete($id){
+        $model = TestPeople::findOne($id);
+
+        if (!$model) {
+            throw new \yii\web\NotAcceptableHttpException("Человек с таким идентификатором ($id) не найден");
+        }
+
+        TestPhonesNumber::deleteAll(['person_id' => $id]);
+
+        if ($model->delete()) {
+            return ['status' => 'success', 'message' => 'Данные удалены'];
+        } else {
+            return ['status' => 'error' , 'message' => 'Ошибка при удалении'];
+        }
+    }
+}
